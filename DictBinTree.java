ce18161b29944030df25b4d0c757f01f0bdf5d14
Josephine <mexy1991@outlook.com>
2019-04-04 17:05:31 +0200

    commit

--- DictBinTree.java
+++ DictBinTree.java
@@ -1,4 +1,48 @@
-public class DictBinTree{
+public class DictBinTree implements Dict{
+
+
+	public DictBinTree(){
+		DictBinTree newTree = new DictBinTree();
+	
+	}
+
+
+	public void insert(int k){
+	//Indsætter nøglen k i træet
+
+	int y = null;
+	int x = T.root;
+	while(x != null){
+	y=x;
+	if(z.key < x.key)
+		x= x.left;	
+	else
+		x = x.right;
+	}
+	z.p = y;
+	
+
+	if (y==null)
+		T.root = z; // tree T is empty
+	else if(z.key < y.key)
+		y.left = z;
+	else 
+		y.right=z;
+	}
+
+
+
+	    public boolean search(int k){
+		if(x==null || k==x.key)
+			return x;
+		if(k < x.key)
+			return treeSearch(x.left, k);
+		else
+			return treeSearch(x.right, k)
+		
+		
+		}
+
 
 	
 }
